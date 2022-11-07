07703ea244c46f374d49d9667b4f17dcde4760f0
Kim Wexler <sft365@gmail.com>
2022-11-07 15:31:35 +0000

    4.2 refactor complete

--- part4/blog_backend/index.js
+++ part4/blog_backend/index.js
@@ -1,20 +1,10 @@
-require("dotenv").config();
-const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const blogsRouter = require("./controllers/blogs");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose
@@ -26,20 +16,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
